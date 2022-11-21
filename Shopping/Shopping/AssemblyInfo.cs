c554d508a907550a65076b22661f1acbdfe18ea7
Jessica Alves <jessica@yoursoft.com.br>
2022-11-21 16:14:34 -0300

    okay

--- Shopping/Shopping/AssemblyInfo.cs
+++ Shopping/Shopping/AssemblyInfo.cs
@@ -1,3 +1,8 @@
-﻿using Xamarin.Forms.Xaml;
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+[assembly: ExportFont("Montserrat-Light.ttf", Alias = "MontserratLight")]
+[assembly: ExportFont("Montserrat-Regular.ttf", Alias = "MontserratRegular")]
+[assembly: ExportFont("Montserrat-Bold", Alias = "MontserratBold")]
+[assembly: ExportFont("Montserrat-SemiBold.ttf", Alias = "MontserratSemiBold")]
