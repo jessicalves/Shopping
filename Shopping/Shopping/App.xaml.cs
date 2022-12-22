b21549eb45284cee45cda4e66dfa5d08fb6ec20e
Jessica Alves <jessica@suasvendas.com>
2022-12-22 17:07:42 -0300

    teste unitario

--- Shopping/Shopping/App.xaml.cs
+++ Shopping/Shopping/App.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
 
 namespace Shopping
 {
@@ -10,12 +13,17 @@ namespace Shopping
         {
             InitializeComponent();
 
-            Device.SetFlags(new string[] {"Shapes_Experimental"});
+            Xamarin.Forms.Device.SetFlags(new string[] {"Shapes_Experimental"});
             MainPage = new Menu();
         }
 
         protected override void OnStart ()
         {
+            AppCenter.Start("ios={Your App secret here};" +
+                  "uwp={Your UWP App secret here};" +
+                  "android={Your Android App secret here};" +
+                  "macos={Your macOS App secret here};",
+                  typeof(Analytics), typeof(Crashes));
         }
 
         protected override void OnSleep ()
